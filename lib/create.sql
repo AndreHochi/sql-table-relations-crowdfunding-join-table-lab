e2c2bd61da0c2054733faa40573bf4ad3654ccd0
Andrehochi <andrehochiwork@gmail.com>
2021-03-06 10:52:13 -0800

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,20 @@
+CREATE TABLE projects (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    category TEXT,
+    funding_goal INT,
+    start_date INT,
+    end_date INT);
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INT
+);
+
+CREATE TABLE pledges (
+    id INTEGER PRIMARY KEY,
+    amount INTEGER,
+    user_id INTEGER,
+    project_id INTEGER
+);
